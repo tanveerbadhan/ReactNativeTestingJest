cac13d11fc0e9ce1da726ec113b0af16133b5ab6
Tanveer Singh <tanveerbadhan@gmail.com>
2025-04-07 16:51:55 -0400

    first snapshop test

--- App.tsx
+++ App.tsx
@@ -3,14 +3,18 @@ import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import Navigation from './src/navigation/Navigation';
 import { persistor, store } from './src/redux/store';
+import { Text, View } from 'react-native';
 
 const App = () => {
   return (
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <Navigation />
-      </PersistGate>
-    </Provider>
+    // <Provider store={store}>
+    //   <PersistGate loading={null} persistor={persistor}>
+    //     <Navigation />
+    //   </PersistGate>
+    // </Provider>
+    <View>
+      <Text>I m a Snapshot</Text>
+    </View>
   );
 };
 
